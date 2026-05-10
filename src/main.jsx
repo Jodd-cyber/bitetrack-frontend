2ed02c89261f5c41d907d8ab8195d6c6022539fb
Akshat Batra <batra.akshat25@gmail.com>
2026-05-10 22:23:59 +0530

    Bootstrap OAuth auth before React mounts

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { ThemeProvider } from './context/ThemeContext.jsx';
+import bootstrapAuthFromUrl from "./utils/authBootstrap";
 import "./index.css";
 import App from "./App";
+
+bootstrapAuthFromUrl();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
