660ef687e0a51d0046a79177df275ee14f6fdf67
Akshat Batra <batra.akshat25@gmail.com>
2026-05-11 21:58:29 +0530

    + button

--- src/pages/Ledger.jsx
+++ src/pages/Ledger.jsx
@@ -897,7 +897,7 @@ function Ledger() {
                 <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
                 </svg>
-                + Add New Order
+                 Add New Order
               </span>
               <span className="gradient-container">
                 <span className="gradient"></span>
